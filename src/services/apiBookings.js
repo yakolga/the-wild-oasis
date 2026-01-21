3f6e00320a345b1ae4ab96e8ee581f47871fb0c9
Olga Kulczycka <olga@webjaksklep.eu>
2026-01-21 20:50:31 +0100

    feat: api-side filtering & api-side sorting

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -1,6 +1,24 @@
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
+export async function getBookings({filter, sortBy}) {
+  let query = supabase.from("bookings").select("id, created_at, startDate, endDate, numNights, numGuests, status, totalPrice, cabins(name), guests(*)");
+
+  //Filter
+  if (filter) query = query.eq(filter.field, filter.value);
+
+  if (sortBy) query = query.order(sortBy.field, {ascending: sortBy.direction === "asc"});
+
+  const {data, error} = await query;
+
+  if (error) {
+    console.error(error);
+    throw new Error("Could not fetch bookings");
+  }
+
+  return data
+}
+
 export async function getBooking(id) {
   const { data, error } = await supabase
     .from("bookings")
