2e80de52b193128e4085cf1574e30ff37250a7ae
Olga Kulczycka <olga@webjaksklep.eu>
2026-01-29 20:06:04 +0100

    feat: user update

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -4,7 +4,6 @@ import MainNav from "./MainNav";
 import Uploader from "../data/Uploader";
 
 const StyledSidebar = styled.aside`
-    background-color: var(--color-grey-100);
     padding: 3.2rem 2.4rem;
     border-right: 1px solid var(--color-grey-100);
     grid-row: 1 / -1;
