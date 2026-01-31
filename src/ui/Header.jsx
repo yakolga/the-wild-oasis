b74d514e1bab86d32e7a4bd0349e224f42c37b37
Olga Kulczycka <olga@webjaksklep.eu>
2026-01-31 22:59:20 +0100

    feat: dark mode

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,6 +5,7 @@ import UserAvatar from "../features/authentication/UserAvatar";
 const StyledHeader = styled.header`
     padding: 1.2rem 4.8rem;
     border-bottom: 1px solid var(--color-grey-100);
+    background-color: var(--color-grey-50);
     display: flex;
     justify-content: flex-end;
     align-items: center;
