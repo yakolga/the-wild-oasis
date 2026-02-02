87b7ef19e19c323254c44936f15aa5fe43e33f08
Olga Kulczycka <olga@webjaksklep.eu>
2026-02-02 19:03:32 +0100

    feat: dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,14 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statistics</div>
+      <div>Activity</div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout;
